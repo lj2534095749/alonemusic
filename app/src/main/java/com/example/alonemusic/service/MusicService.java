59081d31697de1cf81cf8dc90a3e2a3bd060c941
lj2534095749 <2534095749@qq.com>
2020-06-19 15:43:47 +0800

    v2版本 MediaPlayer优化

--- app/src/main/java/com/example/alonemusic/service/MusicService.java
+++ app/src/main/java/com/example/alonemusic/service/MusicService.java
@@ -8,6 +8,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -48,29 +49,38 @@ public class MusicService extends Service {
         isPlayingButton = intent.getBooleanExtra("isPlayingButton", false);
         mediaPlayer = MusicPlayer.getMusicPlayer();
 
+        Bundle bundle = new Bundle();
+        bundle.putBoolean("isPlayingButton", true);
         Intent clickIntent = new Intent(this, MusicActivity.class);
+        clickIntent.putExtras(bundle);
         PendingIntent clickPI = PendingIntent.getActivity(this, 1, clickIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         NotificationChannel notificationChannel =
                 new NotificationChannel("musicNotification", "Music", NotificationManager.IMPORTANCE_DEFAULT);
         NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         notificationManager.createNotificationChannel(notificationChannel);
-        //Icon icon = new Icon(R.drawable.ic_luo_24);
-        //Notification.Action action = new Notification.Action.Builder(R.drawable.ic_luo_24, "BUTTON", clickPI);
+        String contentText = app.getCurrentMusicNameList().get(app.getCurrentMusicPosition());
         Notification.Builder builder = new Notification.Builder(getApplicationContext(), "musicNotification")
-                .setContentText("正在播放：" + app.getCurrentMusicNameList().get(app.getCurrentMusicPosition()))
+                .setContentTitle("ALoneMusic")
+                .setContentText("正在播放......")
                 .setSmallIcon(R.drawable.ic_luo_24)
                 .setContentIntent(clickPI)
                 .setProgress(mediaPlayer.getDuration(), mediaPlayer.getCurrentPosition(),false)
-                .setOngoing(true);
+                .setWhen(System.currentTimeMillis());
 
         if(isPlayingButton == false){
             play(currentMusicPosition);
-            mediaPlayer.start();
         }
 
         Notification notification = builder.build();
         notification.flags = notification.FLAG_FOREGROUND_SERVICE;
         notificationManager.notify(1, notification);
+        //startForeground(1, notification);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mediaPlayer.start();
+            }
+        });
 
         Timer timer = new Timer();//时间监听器
         timer.schedule(new TimerTask() {
@@ -81,7 +91,7 @@ public class MusicService extends Service {
                 notificationManager.notify(1, notification);
             }
             }
-        }, 100, 50);
+        }, 0, 50);
 
         return musicBinder;
     }
@@ -120,11 +130,11 @@ public class MusicService extends Service {
         try {
             mediaPlayer.reset();
             mediaPlayer.setDataSource(currentMusicList.get(position));
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
+            mediaPlayer.prepareAsync();
+        } catch (IOException | IllegalStateException e) {
             e.printStackTrace();
         }
+        //mediaPlayer.start();
     }
 
     @Override
